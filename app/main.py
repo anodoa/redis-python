0c3e560ffa28e6a48cf379410142c15f24081715
anodoa <alex.efimov.1998@gmail.com>
2025-08-07 00:08:40 +0500

    Respond to a PING

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
-import socket  # noqa: F401
+import socket  
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    with socket.create_server(("localhost", 6379), reuse_port=True) as server_socket:
+
+        connection, _ = server_socket.accept() # wait for client
+        #print(f"accepted connection from {address}")
+
+        connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
